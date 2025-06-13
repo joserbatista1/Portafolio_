a9f0397a66ca950b93c875ad67728e9bfad1c3d9
joserbatista1 <149434440+joserbatista1@users.noreply.github.com>
2025-06-13 08:45:53 -0400

    Agregar imagenes al proyecto de POS SYSTEM

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -1,3 +1,14 @@
-export default function Proyectos(){
+import Imagen from "../components/Imagen";
 
+export default function Proyectos() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl mb-4 text-center">POS SYSTEM</h1>
+
+      <Imagen src="/menuBienvenida.png" title="Menú de Bienvenida" />
+      <Imagen src="/ventanaVentas.png" title="Pantalla de Ventas" />
+      <Imagen src="/inventarioProductos.png" title="Inventario" />
+
+    </div>
+  );
 }
